34d9b94810b7cb68eadd47b7ba2fc5fb8bbad7fa
Joshua Sung <yuniksung@gmail.com>
2020-10-12 17:05:25 -0500

    form working fine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
 var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// IMPORTANT: PLACE THIS FUNCTION BEFORE EventListner
+// My note: PLACE THIS FUNCTION BEFORE addEventListner
 var createTaskHandler = function(event) {
 
   event.preventDefault();
-
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  console.log(taskTypeInput);
+  // Create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-}
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+  
+};
 
 formEl.addEventListener("submit", createTaskHandler);
 
