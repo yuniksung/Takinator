e2ccbabdab680d018388be3beea342340b6c1ef3
Joshua Sung <yuniksung@gmail.com>
2020-10-12 02:56:20 -0500

    4.2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
 // IMPORTANT: PLACE THIS FUNCTION BEFORE EventListner
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
-
+formEl.addEventListener("submit", createTaskHandler);
 
 
 
